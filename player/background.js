b3fa73e975265bc53ffb876f1e462672906d6d03
zephyrisalive <zephyrisalive@gmail.com>
2013-07-09 01:28:34 +0800

    add / to replace with space regexp

--- player/background.js
+++ player/background.js
@@ -8,7 +8,7 @@ player_url = 'http://www.xiami.com/song/playlist/id/{album_id}/type/1';
 playlist = [];
 
 formatString = function(name_string) {
-  return name_string.toLowerCase().replace(/\"/g, '').replace(/[\-\|&@#。·.:,]/g, " ").replace(/\s{0,3}(\[.+\]|\(.+\))$/, "").replace(/[\s]{2,}/g, ' ').replace(/(^\s|\s$)/g, '');
+  return name_string.toLowerCase().replace(/\"/g, '').replace(/[\-\|&@#。·.:,/]/g, " ").replace(/\s{0,3}(\[.+\]|\(.+\))$/, "").replace(/[\s]{2,}/g, ' ').replace(/(^\s|\s$)/g, '');
 };
 
 getAlbumId = function(request_album_name, request_performers_in_array, link_tags) {
